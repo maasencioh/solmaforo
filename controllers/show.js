674f8ea3ab024737419d02e44d870136c22cbf90
maasencioh <maasencioh@gmail.com>
2016-02-06 18:13:22 -0500

    refactor: migrate from postgres to google

--- controllers/show.js
+++ controllers/show.js
@@ -4,24 +4,10 @@ var models = require('../models/models');
 
 // GET /show/
 exports.basic = function(req, res) {
-    models.Place.findAll().then(function(place) {
-        models.Data.findAll().then(function (data) {
-            for (var i = 0; i < data.length; i ++)
-                for (var j = 0; j < place.length; j ++)
-                    if (data[i].sit_id === place[j].sit_id)
-                        data[i].sit_name = place[j].sit_name;
-            res.render('show', {data: data});
-        });
-    });
+    res.render('show', {data: undefined});
 };
 
 // GET /show/:place
 exports.places = function(req, res) {
-    models.Place.findOne({where:{sit_id: req.place}}).then(function(place) {
-        models.Data.findAll({where:{sit_id: req.place}}).then(function (data) {
-            for (var i = 0; i < data.length; i ++)
-                data[i].sit_name = place.sit_name;
-            res.render('show', {data: data});
-        });
-    });
+    res.render('show', {data: undefined});
 };
--- controllers/upload.js
+++ controllers/upload.js
@@ -7,6 +7,8 @@ exports.index = function(req, res) {
     res.render('upload');
 };
 
+var re = /([LMCJVSD])\|(2[0-9]\.2[0-9]\.4[0-9])\|2[0-9]:2[0-9]:2[0-9]\s*T:([0-9]+\.[0-9])\|?H:([0-9]+)%\|?UV:([0-9]+)/gm;
+
 exports.upload = function(req, res) {
     if (req.file) {
 
@@ -16,40 +18,49 @@ exports.upload = function(req, res) {
             var sede = /Sede ([0-9])/m.exec(data);
             if (sede)
                 sede = sede[1];
-            var re = /([LMCJVSD])\|(2[0-9])\.(2[0-9])\.(4[0-9])\|(2[0-9]):(2[0-9]):(2[0-9])\s*T:([0-9]+\.[0-9])\|?H:([0-9]+)%\|?UV:([0-9]+)/gm;
             var m;
             while ((m = re.exec(data)) !== null) {
                 if (m.index === re.lastIndex) {
                     re.lastIndex++;
                 }
-                var date = new Date();
-                date.setFullYear(m[4], m[3], m[5]);
-                date.setHours(0, 0, 0, 0);
 
-                models.Data.findOne({where:{sit_id: sede, dat_date: date}})
-                    // when the date exist
-                    .then(function(data) {
-                        data.increment({
-                            dat_uv: Number(m[10]),
-                            dat_hr: Number(m[9]),
-                            dat_temp: Number(m[8]),
-                            dat_count: 1
-                        })
-                    })
+                var date = m[2];
 
-                    // when the date didn't exist
-                    .catch(function(err) {
-                        models.Data.create({
-                            sit_id: Number(sede),
-                            dat_uv: Number(m[10]),
-                            dat_hr: Number(m[9]),
-                            dat_temp: Number(m[8]),
-                            dat_date: date,
-                            dat_count: 1
-                        }).then(function (err) {
-                            if (err) throw err;
-                        });
-                    });
+                models.store.get(date, (function(err, new_date) {
+                    if (err) throw err;
+
+                    // if date don't exist
+                    if (new_date === undefined)
+                        models.store.set(date, {
+                            sede: {
+                                count: 1,
+                                uv: m[4],
+                                hr: m[3],
+                                temp: m[2]
+                            }
+                        }, function (err, key) { if (err) throw err; });
+
+                    // if the date already exist
+                    else {
+                        // and the sede also exist
+                        if (new_date.sede) {
+                            new_date.sede.count += 1;
+                            new_date.sede.uv += m[4];
+                            new_date.sede.hr += m[3];
+                            new_date.sede.temp += m[2];
+                        }
+                        // and the sede don't exist
+                        else
+                            new_date.sede = {
+                                count: 1,
+                                uv: m[4],
+                                hr: m[3],
+                                temp: m[2]
+                            };
+
+                        models.store.set(date, new_date.sede, function (err, key) { if (err) throw err; });
+                    }
+                })(sede, date, m));
             }
         });
 
